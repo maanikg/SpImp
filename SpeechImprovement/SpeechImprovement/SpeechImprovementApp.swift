a506648795ee652dd331a8e8aa688d5f06d28355
Maanik Gogna <gognamaanik@gmail.com>
2023-05-03 18:39:06 -0400

    adding swift files

---
+++ SpeechImprovement/SpeechImprovement/SpeechImprovementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeechImprovementApp.swift
+//  SpeechImprovement
+//
+//  Created by Maanik Gogna on 2023-05-03.
+//
+
+import SwiftUI
+
+@main
+struct SpeechImprovementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
