c9f6f45a715c5509951a7a28e640decf7294a012
TyYan03 <tylery202@gmail.com>
2023-05-10 12:15:38 -0400

    Recording screen improvements

--- SpeechImprovement/SpeechImprovement/RecordScreen.swift
+++ SpeechImprovement/SpeechImprovement/RecordScreen.swift
@@ -6,95 +6,125 @@
 ////
 import SwiftUI
 import Foundation
+import UIKit
+import AVKit
 
-/*
- When finished recording, we need to make a new Score struct that will store the audio and other attributes of the recording
- 
- */
+class StopWatch {
+    var startTime: Date?
+    var timer: Timer?
+    var curTime: TimeInterval = 0.0
+    
+    func start() {
+        startTime = Date()
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) {
+            timer in self.curTime = -(self.startTime?.timeIntervalSinceNow ?? 0.0)
+        }
+    }
+    
+    func stop() {
+        timer?.invalidate()
+        timer = nil
+        startTime = nil
+    }
+    
+    func reset() {
+        stop()
+        curTime = 0.0
+    }
+}
 
-//import AppKit
-//
-//class StopWatch {
-//    var startTime: Date?
-//    var timer: Timer?
-//    var curTime: TimeInterval = 0.0
-//    
-//    func start() {
-//        startTime = Date()
-//        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) {
-//            timer in self.curTime = -(self.startTime?.timeIntervalSinceNow ?? 0.0)
-//        }
-//    }
-//    
-//    func stop() {
-//        timer?.invalidate()
-//        timer = nil
-//        startTime = nil
-//    }
-//    
-//    func reset() {
-//        stop()
-//        curTime = 0.0
-//    }
-//}
-//
-//
-//
-struct RecordScreen: View {
-//    
+
+
+struct Record: View {
+    
     @State private var recording: Bool = false
-//    @State private var timeLabel:
-//        String = "00:00"
-//    @State var stopwatch = StopWatch()
-//   
-//    var tapGesture: some Gesture {
-//        TapGesture()
-//            .onEnded {
-//                withAnimation {
-//                    recording = !recording;
-//                    if(recording) {
-//                        stopwatch.start()
-//                        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {  _ in
-//                            updateElapsedTime()
-//                        }
-//                    }
-//                    else {
-//                        stopwatch.stop()
-//                        // Create pop up window to go to score or to keep recording
-//                        let alert = NSAlert()
-//                            //alert.messageText = "Variable is false"
-//                            alert.informativeText = "Are you done with your recording?"
-//                            alert.alertStyle = .warning
-//                            alert.addButton(withTitle: "Continue Recording")
-//                            alert.addButton(withTitle: "Done")
-//                            
-//                            // Show the alert as a modal dialog
-//                            let response = alert.runModal()
-//                            
-//                            // Check which button was clicked
-//                            if response == NSApplication.ModalResponse.alertFirstButtonReturn {
-//                                // Dismiss button clicked
-//                                //print("Dismiss button clicked")
-//                                stopwatch.start()
-//                            } else if response == NSApplication.ModalResponse.alertSecondButtonReturn {
-//                                // Go to new page button clicked
-//                                //print("Go to new page button clicked")
-//                                // TODO: Navigate to new page here
-//                                let fileURL = URL(fileURLWithPath: "Score.swift")
-//                                        
-//                                        // Open the file with the default application
-//                                NSWorkspace.shared.open(fileURL)
-//                            }
-//                    }
-//                        
-//                }
-//            }
-//    }
-//    
-//    func updateElapsedTime() {
-//        timeLabel = String(format: "%.1f", stopwatch.curTime)
-//    }
-//    
+    @State private var timeLabel:
+        String = "00:00"
+    @State var stopwatch = StopWatch()
+    @State var session: AVAudioSession!
+    @State var recorder: AVAudioRecorder!
+    @State var alert = false
+    @State var audios: [URL] = []
+    @State var isPresented = false
+    @State var shouldNavigate = false
+    @State private var temp: Bool = true
+   
+    var tapGesture: some Gesture {
+        TapGesture()
+            .onEnded {
+                withAnimation {
+                    recording = !recording;
+                    if(recording) {
+                        do {
+                            stopwatch.start()
+                            let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                            
+                            // same file name...
+                            // so were updating based on audio count...
+                            let filName = url.appendingPathComponent("myRcd\(self.audios.count + 1).m4a")
+                            
+                            let settings = [
+                                
+                                AVFormatIDKey : Int(kAudioFormatMPEG4AAC),
+                                AVSampleRateKey : 12000,
+                                AVNumberOfChannelsKey : 1,
+                                AVEncoderAudioQualityKey : AVAudioQuality.high.rawValue
+                                
+                            ]
+                            
+                            self.recorder = try AVAudioRecorder(url: filName, settings: settings)
+                            self.recorder.record()
+                            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {  _ in
+                                updateElapsedTime()
+                            }
+                        }
+                        catch {
+                            print(error.localizedDescription)
+                        }
+                    }
+                    else {
+                        stopwatch.stop()
+                        self.recorder.pause()
+                        self.isPresented = true
+                        // Create pop up window to go to score or to keep recording
+                        let alert = UIAlertController(title: "Recording", message: "Would you like to continue recording?", preferredStyle: .alert)
+                        let doneAction = UIAlertAction(title: "Done", style: .default) { (_) in
+                                // Do something when OK is tapped
+                                // This can be left blank if you just want the window to dismiss
+                            
+                                 self.recorder.stop()
+                                 self.getAudios()
+                                NavigationLink(destination: FinalScore(), isActive: $temp) {
+                                    EmptyView()
+                                }
+                            }
+                        
+                        let continueAction = UIAlertAction(title: "Continue Recording", style: .default) { (_) in
+                            
+                            self.recorder.record()
+                            stopwatch.start()
+                            recording = true
+                        }
+                        
+                        alert.addAction(doneAction)
+                        alert.addAction(continueAction)
+                            // Present the alert controller
+                        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                           let sceneDelegate = windowScene.delegate as? SceneDelegate,
+                           let viewController = sceneDelegate.window?.rootViewController {
+                               viewController.present(alert, animated: true, completion: nil)
+                        }
+                           
+                
+                        }
+                }
+            }
+    }
+    
+    func updateElapsedTime() {
+        timeLabel = String(format: "%.1f", stopwatch.curTime)
+    }
+    
     var body: some View {
         VStack{
             Text(!recording ? "Press to start recording" : "Press to finish recording")
@@ -102,10 +132,36 @@ struct RecordScreen: View {
                 .bold()
             Image(!recording ? "notRecording" : "recording")
                 .shadow(radius: 40)
-//                .gesture(tapGesture)
-//            Text(timeLabel)
-//                .font(.title)
-//                .bold()
+                .gesture(tapGesture)
+            Text(timeLabel)
+                .font(.title)
+                .bold()
+            if shouldNavigate {
+                NavigationLink(destination: FinalScore()) {
+                    
+                }
+                .frame(width: 0, height: 0)
+                .hidden()
+                
+            }
+        }
+        .onAppear {
+            do {
+                self.session = AVAudioSession.sharedInstance()
+                try self.session.setCategory(.playAndRecord)
+                
+                self.session.requestRecordPermission { (status) in
+                    if !status {
+                        self.alert.toggle()
+                    }
+                    else {
+                        self.getAudios()
+                    }
+                }
+            }
+            catch {
+                print(error.localizedDescription)
+            }
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -113,13 +169,65 @@ struct RecordScreen: View {
             LinearGradient(colors: [Color.white, !recording ? Color.blue : Color.indigo], startPoint: .top, endPoint: .bottom)
         )
     }
-//        
+    
+    func getAudios() {
+        do {
+            let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+                        
+                        // fetch all data from document directory...
+                        
+            let result = try FileManager.default.contentsOfDirectory(at: url, includingPropertiesForKeys: nil, options: .producesRelativePathURLs)
+                        
+                        // updated means remove all old data..
+                        
+            self.audios.removeAll()
+                        
+            for i in result{
+                            
+                self.audios.append(i)
+            }
+        }
+        catch{
+            print(error.localizedDescription)
+        }
+    }
 }
-//
-//
-//
+
+struct PopUp: View {
+    @Binding var shouldNavigate: Bool
+    @Environment(\.presentationMode) var presentationMode
+    var body: some View {
+        VStack {
+            Text("Pop-Up Window")
+                .font(.title)
+                .padding()
+            
+            Button("Continue Recording") {
+                self.dismiss()
+            }
+            .padding()
+            
+            Button("Done") {
+                self.shouldNavigate = true
+                self.dismiss()
+            }
+            .padding()
+        }
+        .padding()
+        .background(Color.white)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+    }
+    
+    private func dismiss() {
+        self.shouldNavigate = false
+        presentationMode.wrappedValue.dismiss()
+    }
+}
+
+
 struct Record_Previews: PreviewProvider {
     static var previews: some View {
-        RecordScreen()
+        Record()
     }
 }
