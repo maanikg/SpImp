745fbc3bb59a8b85f135780980549efc8544d0e1
maanikg <gognamaanik@gmail.com>
2023-05-22 15:00:32 -0400

    changed app icon

--- SpeechImprovement/SpeechImprovement/CircleImage.swift
+++ SpeechImprovement/SpeechImprovement/CircleImage.swift
@@ -10,11 +10,11 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         //Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-        Image("waveformNew")
-//            .clipShape(Circle())
-//            .overlay{
-//                Circle().stroke(.black, lineWidth: 5)
-//            }
+        Image("waveform")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.black, lineWidth: 5)
+            }
             .shadow(radius: 20)
     }
 }
