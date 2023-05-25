ecfb167a5b68fa021354472447905c61475bf98f
maanikg <gognamaanik@gmail.com>
2023-05-25 16:30:11 -0400

    calculating duration of time for each category of tone

--- SpeechImprovement/SpeechImprovement/ToneModel.swift
+++ SpeechImprovement/SpeechImprovement/ToneModel.swift
@@ -11,16 +11,19 @@ import CoreML
 import SoundAnalysis
 
 let defaultConfig = MLModelConfiguration()
-let toneClassifier = try! toneModel(configuration: defaultConfig)
+//let toneClassifier = try! toneModel(configuration: defaultConfig)
 //let toneClassifier = try! SpeechImprovement_ML_12(configuration: defaultConfig)
 //let toneClassifier = try! SpeechImprovement_ML_13(configuration: defaultConfig)
 //let toneClassifier = try! SpeechImprovement_ML_14(configuration: defaultConfig)
-//let speedClassifier
-//let volumeClassifier
-//let clarityClassifier
+
+//SpeechImprovement_ML_10 model has 100% training, 93% accuracy (25 iterations, 5.0 sec window duration, 75% window overlap)
+let toneClassifier = try! SpeechImprovement_ML_10(configuration: defaultConfig)
+//let speedClassifier = try! speedModel(configuration: defaultConfig)
+//let volumeClassifier try! volumeModel(configuration: defaultConfig)
+//let clarityClassifier try! clarityModel(configuration: defaultConfig)
 let classifyToneSoundReq = try! SNClassifySoundRequest(mlModel: toneClassifier.model)
 let resultsObserver = ResultsObserver()
-
+var resultDict: [String:Double] = [:]
 
 //TODO: COMMENTED OUT PRINTING FOR CLARITY
 /// An observer that receives results from a classify sound request.
@@ -33,23 +36,30 @@ class ResultsObserver: NSObject, SNResultsObserving {
         // Get the prediction with the highest confidence.
 //        guard let classification = result.classifications.first else { return }
         let classification = result.classifications
-        
-
         // Get the starting time.
         let timeInSeconds = result.timeRange.start.seconds
+        let endTime = result.timeRange.end.seconds
 
         // Convert the time to a human-readable string.
         
         let formattedTime = String(format: "%.2f", timeInSeconds)
         //COMMENTED OUT
-//        print("Analysis result for audio at time: \(formattedTime)")
+        print("Analysis result for audio at time: \(formattedTime) - \(String(format:"%.2f", endTime))")
 
         for classif in classification{
             let percent = classif.confidence * 100.0
             let percentString = String(format: "%.2f%%", percent)
+
+            if let value = resultDict[classif.identifier]{
+                resultDict[classif.identifier] = value + classif.confidence*result.timeRange.duration.seconds
+            }
+            else{
+                resultDict[classif.identifier] = classif.confidence*result.timeRange.duration.seconds
+            }
+
             //COMMENTED OUT
             if (round(percent) != 0){
-//                print("\(classif.identifier): \(percentString) confidence.")
+                print("\(classif.identifier): \(percentString) confidence.")
             }
         }
         
@@ -69,6 +79,7 @@ class ResultsObserver: NSObject, SNResultsObserving {
     /// Notifies the observer when a request is complete.
     func requestDidComplete(_ request: SNRequest) {
         print("The sound classifier request completed successfully!")
+        print(resultDict)
     }
 }
 
